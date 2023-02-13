fbd8b3a58d7b0eef009c204cf1e4efb725332279
murodilla <117287005+ZaylobiddinovMurodilla003@users.noreply.github.com>
2023-02-13 19:15:01 +0500

    Add files via upload
    lesson 7 for array objact

---
+++ lesson-7/script.js
@@ -0,0 +1,48 @@
+"use script";
+
+let arr1 = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  ["sadvni", "eihfjne", "eonkrvb", "oeknrr", "oekfb"],
+  [34, 3434, 343, 45, ["fgdfg", "gdfgdg ", "gdfgdfg"]],
+];
+
+for (let i = 0; i < arr1.length; i++) {
+  console.log(arr1[i]);
+}
+
+// console.log(arr1[5][3]);
+// console.log(arr1[6][4][2]);
+// console.log(arr1);
+
+// const user = {
+//   fName: "murodilla ",
+//   login: "men bu men",
+//   parol: "sen bu sen",
+//   email: "zaylobiddinovmurodilla1@gmail.com",
+//   phoneNumber: "94 328 0661",
+//   year: 2003,
+//   now: 2023,
+
+//   joylashuv: {
+//     viloyat: "Fargona",
+//     tuman: "buvayda",
+//   },
+
+//   age: function () {
+//     let a = this.now - this.year;
+//     return a;
+//   },
+// };
+
+// console.log(user.joylashuv.viloyat);
+// console.log(user.age());
+// console.log(user);
+// console.log(Object.keys(user));
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
